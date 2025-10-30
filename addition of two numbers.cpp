3b9e6a3d55b133adf09889c611ad074d7939ae84
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2025-10-30 11:08:55 +0530

    Add files via upload

---
+++ addition of two numbers.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter a value:");
+	scanf("%d", &a);
+	
+	printf("enter b value:");
+	scanf("%d",&b);
+	
+	c=a+b;
+	printf(" addition of %d and %d = %d",a,b,c);
+	
+	
+}
---
+++ area of triangle.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	float b,h,area;
+	printf("enter base:");
+	scanf("%f",&b);
+	printf("enter height:");
+	scanf("%f",&h);
+	area=(0.5)*b*h;
+	printf("area of triangle:%f",area);
+	return 0;
+	
+}
