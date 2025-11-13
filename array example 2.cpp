f0291efb0bbea3f296f9e314442c7017dab1a6ff
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2025-11-13 15:17:01 +0530

    Add files via upload

--- array example 2.cpp
+++ array example 2.cpp
@@ -1,22 +1,9 @@
-//display student marks of ppsc by using inputted numbers
-#include<stdio.h>
+#inclide<stdio.h>
 int main()
 {
-	int n,i;
-	printf(" Enter array size :");
+	int i,n,student[n];
+	printf(" Enter array size:");
 	scanf("%d",&n);
-	int student[n];
 	
-	printf("enter marks of student:");
-	
-	for(i=0;i<=n;i++)
-	{
-	  scanf("%d",&student[i]);
-    }
-    printf(" Marks of student :");
-    for(i=0;i<n;i++)
-    {
-    	printf(" Marks of students %d is %d\n ",i,student[i]);
-	}
 	
 }
