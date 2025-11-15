724da196aa8f97ed6591ec69704ebf07612ffdd2
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2025-11-15 11:11:36 +0530

    Add files via upload

---
+++ array(sum of elements in array).cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,A[50],sum=0;
+	printf("size of array:");
+	scanf("%d",&n);
+	
+	printf("enter values:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum +A[i];
+	}
+	printf(" sum of elements in array :%d",sum);
+}
