d10c10c17a084646746fcc6884c02a3bd2cc1937
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2025-11-15 11:13:25 +0530

    Add files via upload

---
+++ array(sum of odd index places).cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,A[50],sum=0;
+	printf("size of array:");
+	scanf("%d",&n);
+	
+	printf("enter values:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&A[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(i%2!7=0)
+		{
+			sum=sum +A[i];
+		}
+	}printf("%d",sum);
+}
