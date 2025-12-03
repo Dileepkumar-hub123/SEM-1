b4315c693d31e85ab649aca10c9a6a7af3a4555c
Dileepkumar-hub123 <jdileepkumar1@gmail.com>
2025-12-03 13:41:00 +0530

    Add files via upload

---
+++ function(without return and without parameter).cpp
@@ -0,0 +1,14 @@
+//without return and without parameter//
+#include<stdio.h>
+void add()
+{
+	int a=5,b=7;
+	printf("%d",sum);
+}
+
+int main()
+{
+	int a=5,b=7,sum=0;
+	sum=add(a,b);
+	printf("%d",sum);
+}
